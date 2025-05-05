aadbe431707623a54a16e3efd867da49b2f7ad89
Elin23 <elinshaia23@gmail.com>
2025-05-05 12:41:11 +0300

    Final Version

--- src/hooks/useResponsiveSlides.js
+++ src/hooks/useResponsiveSlides.js
@@ -1,35 +1,34 @@
 import { useState, useEffect } from "react";
 
 const useResponsiveSlides = ({
-  breakpoints = { x_small: 1, small: 2, medium: 3, large: 4 }
-}) => {
-  const [slidesToShow, setSlidesToShow] = useState(breakpoints.large);
+  breakpoints = { x_small: 1, small: 2, medium: 3, large: 4 }}) => {
+  const [displayedSlides, setDisplayedSlides] = useState(breakpoints.large);
 
   useEffect(() => {
-    const observer = new ResizeObserver((entries) => {
+    const Monitor = new ResizeObserver((entries) => {
       for (let entry of entries) {
         const width = entry.contentRect.width;
 
         if (width < 540) {
-          setSlidesToShow(breakpoints.x_small);
+          setDisplayedSlides(breakpoints.x_small);
         } else if (width < 992) {
-          setSlidesToShow(breakpoints.small);
+          setDisplayedSlides(breakpoints.small);
         } else if (width <= 1200) {
-          setSlidesToShow(breakpoints.medium);
+          setDisplayedSlides(breakpoints.medium);
         } else {
-          setSlidesToShow(breakpoints.large);
+          setDisplayedSlides(breakpoints.large);
         }
       }
     });
 
-    observer.observe(document.documentElement);
+    Monitor.observe(document.documentElement);
 
     return () => {
-      observer.disconnect();
+      Monitor.disconnect();
     };
   }, [breakpoints]);
 
-  return slidesToShow;
+  return displayedSlides;
 };
 
 export default useResponsiveSlides;
